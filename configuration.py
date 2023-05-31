a174310fb57ae6117e3b8d5932f9fcde3c38919c
pwgamer <invizible95@gmail.com>
2023-05-31 11:29:33 +0300

    first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://fad4f9ae-a334-4852-8780-0847c8a0fb4c.serverhub.praktikum-services.ru/"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
